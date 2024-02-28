62ab2e4e556594899e7b70acee89951ebc7b4bd6
Oğuzhan Arslantaş <iamoguzhanarslantas@gmail.com>
2024-02-28 19:28:19 +0300

    Add Equatable dependency to author_works_cubit.dart and authors_cubit.dart

--- lib/src/blocs/author_works/author_works_state.dart
+++ lib/src/blocs/author_works/author_works_state.dart
@@ -1,8 +1,11 @@
 part of 'author_works_cubit.dart';
 
 @immutable
-sealed class AuthorWorksState {
+sealed class AuthorWorksState extends Equatable {
   const AuthorWorksState();
+
+  @override
+  List<Object> get props => [];
 }
 
 final class AuthorWorksInitial extends AuthorWorksState {}
@@ -12,9 +15,15 @@ final class AuthorWorksLoading extends AuthorWorksState {}
 final class AuthorWorksLoaded extends AuthorWorksState {
   final AuthorWorksResponse? authorWorksList;
   const AuthorWorksLoaded(this.authorWorksList);
+
+  @override
+  List<Object> get props => [authorWorksList!];
 }
 
 final class AuthorWorksError extends AuthorWorksState {
   final String? errorMessage;
   const AuthorWorksError(this.errorMessage);
+
+  @override
+  List<Object> get props => [errorMessage!];
 }
--- lib/src/blocs/authors/authors_state.dart
+++ lib/src/blocs/authors/authors_state.dart
@@ -1,8 +1,11 @@
 part of 'authors_cubit.dart';
 
 @immutable
-sealed class AuthorsState {
+sealed class AuthorsState extends Equatable {
   const AuthorsState();
+
+  @override
+  List<Object> get props => [];
 }
 
 final class AuthorsInitial extends AuthorsState {}
@@ -12,9 +15,15 @@ final class AuthorsLoading extends AuthorsState {}
 final class AuthorsLoaded extends AuthorsState {
   final AuthorsResponse? authorsList;
   const AuthorsLoaded(this.authorsList);
+
+  @override
+  List<Object> get props => [authorsList!];
 }
 
 final class AuthorsError extends AuthorsState {
   final String? errorMessage;
   const AuthorsError(this.errorMessage);
+
+  @override
+  List<Object> get props => [errorMessage!];
 }
